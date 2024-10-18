3f6850caa486b6e6cab07aa0a67a9d8d7c1deffb
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-18 13:56:25 +0200

    ADD: primo commit es.Palindroma

---
+++ Palindroma/palindroma.js
@@ -0,0 +1,19 @@
+console.log('Palindroma')
+
+
+
+// Palidroma
+//Chiedere all’utente di inserire una parola 
+//Creare una funzione per capire 
+//se la parola inserita è palindroma
+
+
+
+// scrivo la parola palindroma
+
+const inserisciParola = prompt('inserisci una parola') 
+ 
+// scrivo la parola lettera per lettera per controllare se funziona
+
+let parolaDivisa = inserisciParola.split('')
+console.log(parolaDivisa)
