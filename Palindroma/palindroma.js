1fbb31aaf0903d5518d1eb6efb541717781df489
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-18 15:32:51 +0200

    ADD: primo esercizio completato

--- Palindroma/palindroma.js
+++ Palindroma/palindroma.js
@@ -15,14 +15,14 @@ const parola =  prompt ('inserisci una parola');
  
 // scrivo la funzione per verifcare la parola 
 
-let parolaInvertita = parola.split(''); 
+let parolaInvertita = parola.split('').reverse().join(''); 
 console.log (parolaInvertita);
 
 
 // verifico se la parola è palindroma
 
 
-if (parola == parolaInvertita) {
+if (parola === parolaInvertita) {
     console.log ('La parola è palindroma')
 } else {
     console.log ('La parola non è palindroma')
