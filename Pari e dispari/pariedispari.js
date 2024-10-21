9a9be02be80d5c014f62300e28d8ca7fb5a528bb
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-21 13:10:48 +0200

    ADD: correzione pari e dispari

--- Pari e dispari/pariedispari.js
+++ Pari e dispari/pariedispari.js
@@ -15,12 +15,59 @@ console.log ('pari e dispari')
 
 
 
-// chiedo all'utente di scegliere pari o dispari e di scrivere un numero un numero da 1 a 5
+// chiediamo all'utente di inserire 'pari' o 'dispari'
+const sceltaUtente = prompt('Scrivi pari o dispari')
+// chiediamo un numero da 1 a 5
+const numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'))
+console.log('scelta: ', sceltaUtente)
+console.log('numero utente: ', numeroUtente)
+// generare un numero da 1 a 5 per il pc
+const numeroPC = getRandomIntInclusive(1, 5)
+// facciamo la somma dei due numeri
+console.log('numero pc:', numeroPC)
+const somma = numeroPC + numeroUtente
+console.log('somma:', somma)
+// - utente vince SE pari e somma pari
+// - utente vince SE dispari e somma è dispari
+// - altrimenti utente perde
+// verificare chi ha vinto
+let message = 'Hai perso!'
+if (userWins(somma, sceltaUtente)) {
+	message = 'Hai vinto'
+}
+// if (isEven(somma) && sceltaUtente.toLowerCase() === 'pari') {
+// 	message = 'Hai vinto'
+// } else if (!isEven(somma) && sceltaUtente.toLowerCase() === 'dispari') {
+// 	message = 'Hai vinto'
+// }
+// const sommaPariEUtentePari =
+// 	isEven(somma) && sceltaUtente.toLowerCase() === 'pari'
+// const sommaDispariEUtenteDispari =
+// 	!isEven(somma) && sceltaUtente.toLowerCase() === 'dispari'
+// if (sommaPariEUtentePari || sommaDispariEUtenteDispari) {
+// 	message = 'Hai vinto'
+// }
+// stampa messaggio vittoria
+console.log(message)
+function userWins(somma, sceltaUtente) {
+	// user wins when sum is even
+	if (isEven(somma) && sceltaUtente.toLowerCase() === 'pari') {
+		return true
+	}
+	// user wins whne sum is odd
+	if (!isEven(somma) && sceltaUtente.toLowerCase() === 'dispari') {
+		return true
+	}
+	// user loose
+	return false
+}
+function getRandomIntInclusive(min, max) {
+	const minCeiled = Math.ceil(min)
+	const maxFloored = Math.floor(max)
+	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled) // The maximum is inclusive and the minimum is inclusive
+}
+function isEven(num) {
+	return num % 2 === 0
+	// const resto = num % 2
 
-const paridispari = prompt ('scegli pari o dispari')
-const numberUser = prompt ('inserisci un numero da 1 a 5')
-
-// genero un numero random per il computer
-
-const numberPc = Math.floor(Math.random()*5)+1;
-console.log (numberPc)
+}
